e3c3735171f7844ded231fb41b67d7d8518ae538
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-10-16 23:07:25 +0700

    feat: Auth'd user can generate presigned url

--- video/urls.py
+++ video/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.TestView.as_view(), name='temp function'), 
+    path("test/", views.TestView.as_view(), name="temp function"),
+    path(
+        "upload-psurl/",
+        views.UploadPresignedURLView.as_view(),
+        name="Generate Pre-signed URL",
+    ),
 ]
--- video/views.py
+++ video/views.py
@@ -3,13 +3,51 @@ from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 
+import boto3
+from botocore.client import Config
+from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
+
+
 class TestView(GenericAPIView):
     # add this to the endpoints that requires authen
-    permission_classes = [IsAuthenticated, ] 
+    permission_classes = [
+        IsAuthenticated,
+    ]
 
     def get(self, request):
-        
-        return Response(
-            data={'hello': 'world'},
-            status=status.HTTP_200_OK
+        return Response(data={"hello": "world"}, status=status.HTTP_200_OK)
+
+
+class UploadPresignedURLView(GenericAPIView):
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    def get(self, request):
+        load_dotenv()
+        s3 = boto3.client(
+            "s3",
+            region_name=os.environ.get("S3_REGION"),
+            endpoint_url=os.environ.get("S3_RAW_ENDPOINT"),
+            aws_access_key_id=os.environ.get("S3_ACCESS_KEY"),
+            aws_secret_access_key=os.environ.get("S3_SECRET_ACCESS_KEY"),
+            config=Config(s3={"addressing_style": "virtual"}),
         )
+
+        try:
+            return Response(
+                data={
+                    "url": s3.generate_presigned_url(
+                        ClientMethod="put_object",
+                        Params={
+                            "Bucket": os.environ.get("S3_BUCKET_NAME"),
+                            "Key": "Super awesome key",
+                        },
+                    )
+                },
+                status=status.HTTP_200_OK,
+            )
+        except ClientError as e:
+            return Response(data={"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
