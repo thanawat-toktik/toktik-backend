801db0f2352a071577098388592cc83c42971644
thanawinboon <pokepat27@gmail.com>
2023-10-16 20:26:46 +0700

    completed jwt authentication

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -23,7 +23,7 @@ class JWTAuthenticationMiddleware(MiddlewareMixin):
     def get_jwt_user(request):
 
         user_jwt = get_user(request)
-        if user_jwt.is_authenticated():
+        if user_jwt.is_authenticated:
             return user_jwt
         token = request.META.get('HTTP_AUTHORIZATION', None)
 
