fa3410cbf52f3f8fe57e30a43a70604cfb917eb5
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-10-18 02:26:39 +0700

    feat: updates DB when video upload done

--- video/models.py
+++ video/models.py
@@ -3,12 +3,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Video(models.Model):
     id = models.AutoField(primary_key=True)
     # uploader = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     upload_timestamp = models.DateTimeField(default=timezone.now)
-    
-    s3_url_raw = models.CharField(max_length=60)
-    s3_url_converted = models.CharField(max_length=60)
 
+    title = models.CharField(max_length=50, default="")
+    caption = models.CharField(max_length=100, blank=True)
+    s3_key = models.CharField(max_length=36, blank=False, unique=True, default="")
