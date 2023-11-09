34a9d5ce0409b866e88c48fcba40af388697647a
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-09 22:40:34 +0700

    feat: get for checking if liked

--- video/views/like_views.py
+++ video/views/like_views.py
@@ -4,11 +4,23 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from video.serializers.like_serializers import CreateLikeSerializer
+from video.models import Like
 
 
 class LikeVideo(GenericAPIView):
     serializer_class = CreateLikeSerializer
     permission_classes = [IsAuthenticated, ]
+
+    def get(self, request):
+        user_id = request.user.id
+        video_id = request.query_params.get("videoId", None)
+        if video_id is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"message": "videoId not provided"})
+        try:
+            like_entry = Like.objects.get(user=user_id, video=video_id)
+            return Response(status=status.HTTP_200_OK, data={"isLiked": like_entry.isLiked})
+        except Like.DoesNotExist:
+            return Response(status=status.HTTP_200_OK, data={"isLiked": False})
 
     def post(self, request):
         data = request.data
