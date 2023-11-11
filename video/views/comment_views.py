2bfa5bff66bcc0a27ba15921d949d2d9fe7a1c55
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-11 22:57:05 +0700

    feat: added fetch comment endpoint

--- video/views/comment_views.py
+++ video/views/comment_views.py
@@ -3,12 +3,24 @@ from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 
+from video.models import Comment
 from video.serializers.comment_serializers import CreateCommentSerializer
 from notification.views import create_notification
 
+
 class PostComment(GenericAPIView):
     serializer_class = CreateCommentSerializer
-    permission_classes = [IsAuthenticated,]
+    permission_classes = [IsAuthenticated, ]
+
+    def get(self, request):
+        video_id = request.query_params.get("video_id", None)
+        if not video_id:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"message": "video_id not provided"})
+
+        comments = Comment.objects.filter(video_id=video_id).all()
+        formed_comments = [{"user": comment.user.username, "content": comment.content}
+                           for comment in comments]
+        return Response(status=status.HTTP_200_OK, data=formed_comments)
 
     def post(self, request):
         data = request.data
