6f80e1cbf04ffbfbe2cedc6bf0f45d10664734ee
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-11 23:20:35 +0700

    feat: custom logic for login

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+    # TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
-from authentication.views import UserCreateView
-
+from authentication.views import UserCreateView, CustomObtainTokenPairView
 
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name='register'),
-    path('jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/create/', CustomObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
