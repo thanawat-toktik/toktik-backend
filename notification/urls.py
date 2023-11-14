4dca11990b85c02f559f10bd28a7897eab55ba8c
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-15 01:06:02 +0700

    feat: added is_seen updating and queue to notifs

--- notification/urls.py
+++ notification/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import FetchNotifications
+from .views import FetchNotifications, UpdateSeen
 
 urlpatterns = [
     path("fetch/", FetchNotifications.as_view(), name="Fetch notifications"),
+    path("update-seen/", UpdateSeen.as_view(), name="Update seen"),
 ]
--- toktik_backend/settings.py
+++ toktik_backend/settings.py
@@ -66,9 +66,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "authentication.middleware.JWTAuthenticationMiddleware",
-    'django.middleware.cache.UpdateCacheMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 REST_FRAMEWORK = {
